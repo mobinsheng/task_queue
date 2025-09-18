f544e1b697607fdf37b09c4b2a65ddc1ab71a483
mobinsheng <574923692@qq.com>
2025-09-19 01:36:25 +0800

    Update task_queue.h

--- task_queue.h
+++ task_queue.h
@@ -1,6 +1,13 @@
 /*
- *  Created by mobinsheng.
+ *  Copyright mobinsheng. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
  */
+
 #ifndef _LAZY_TASK_QUEUE_H_
 #define _LAZY_TASK_QUEUE_H_
 #include <assert.h>
